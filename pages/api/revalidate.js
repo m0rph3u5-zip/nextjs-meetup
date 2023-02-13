1fa072f4f171aaa9bd5675dcc18281b605eb8ac3
Savino <savino.zippone@ubilot.com>
2023-02-13 17:39:33 +0100

    fix revalidate

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
     const urlToRevalidate = req.body.url;
-    urlToRevalidate && (await res.revalidate(urlToRevalidate));
+    urlToRevalidate && (await res.unstable_revalidate(urlToRevalidate));
     return res.json({ revalidated: true, revalidatedUrl: urlToRevalidate });
   } catch (err) {
     return res.status(500).send({ message: 'Error revalidating', error: err });
